87623fca9a09782ac73fb8c687c95a3f7bfcfcb2
AlyssaU3 <alys4321c@gmail.com>
2025-09-02 15:24:42 -0400

    exersise 7.7 done

--- src/create_tuple.py
+++ src/create_tuple.py
@@ -1,7 +1,8 @@
 # Write your solution here
 def create_tuple(x: int, y: int, z: int):
-    pass
-
+    values = [x, y, z]
+    my_tuple=(min(values), max(values), x+y+z)
+    return my_tuple
 
 if __name__ == "__main__":
     print(create_tuple(5, 3, -1))
